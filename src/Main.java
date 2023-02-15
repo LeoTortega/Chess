da5503aaecd5c72413d2cad9d54127c756e5ad91
Leonardo Torres Ortega <leonardo.tortega@outlook.com>
2023-02-15 10:28:02 -0300

    Camada xadrez e print board

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
 
-        Board board = new Board(8, 8);
-        
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(match.getPieces());
+
     }
 }
