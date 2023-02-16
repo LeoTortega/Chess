9de7c7dce6138d11902c1f2c1f8ed6c8dcd15aa6
Leonardo Torres Ortega <leonardo.tortega@outlook.com>
2023-02-16 11:40:32 -0300

    Movimento torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,6 +30,8 @@ public class ChessMatch {
         Position target = targetPosition.toPosition();
 
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
+
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
@@ -48,6 +50,12 @@ public class ChessMatch {
         }
         if(!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible move for choosen piece");
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target) {
+        if(!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to chosen position");
         }
     }
 
